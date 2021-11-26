217e57ef6544a834921a13fcb7ac5b0a37612411
Robin Lake <87996036+RobinTempusEx@users.noreply.github.com>
2021-11-26 11:17:31 -0700

    placeholder image

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import t4tlogo from '../../assets/t4tlogo.png';
+
+const Home = () => {
+    return (
+        <div>
+            <img src={t4tlogo} alt="t4t logo" />
+        </div>
+    )
+}
+
+export default Home;
